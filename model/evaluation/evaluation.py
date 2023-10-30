86a83326130d207caebd6c9db926463fea6b53d1
Alireza Ziaee <alrz1999@gmail.com>
2023-10-31 00:38:08 +0330

    Add evaluation

--- model/evaluation/evaluation.py
+++ model/evaluation/evaluation.py
@@ -1,6 +1,5 @@
 import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_auc_score, balanced_accuracy_score, matthews_corrcoef, roc_curve
 
 def evaluate(true_labels, predicted_labels):
     accuracy = accuracy_score(true_labels, predicted_labels)
@@ -13,6 +12,18 @@ def evaluate(true_labels, predicted_labels):
     print("Recall:", recall)
     print("F1-score:", f1)
     print("Confusion Matrix:\n", confusion)
+
+    # Calculate the AUC-ROC (for binary classification)
+    fpr, tpr, thresholds = roc_curve(true_labels, predicted_labels)
+    roc_auc = roc_auc_score(true_labels, predicted_labels)
+
+    # Calculate Balanced Accuracy
+    balanced_acc = balanced_accuracy_score(true_labels, predicted_labels)
+    print("Balanced Accuracy:", balanced_acc)
+
+    # Calculate Matthews Correlation Coefficient
+    mcc = matthews_corrcoef(true_labels, predicted_labels)
+    print("Matthews Correlation Coefficient (MCC):", mcc)
 
 
 def main():
