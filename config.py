c2832465b7c79e77689d2225d414714c04e713c3
Alireza Ziaee <alrz1999@gmail.com>
2023-11-16 21:30:32 +0330

    Update config

--- config.py
+++ config.py
@@ -20,5 +20,8 @@ BOW_SAVE_PREDICTION_DIR = os.path.join(PROJECT_ROOT, 'output', 'prediction', 'Bo
 MLP_SAVE_MODEL_DIR = os.path.join(PROJECT_ROOT, 'output', 'model', 'mlp')
 MLP_SAVE_PREDICTION_DIR = os.path.join(PROJECT_ROOT, 'output', 'prediction', 'mlp')
 
+# Define paths for saving models and predictions
+KERAS_SAVE_PREDICTION_DIR = os.path.join(PROJECT_ROOT, 'output', 'prediction', 'keras')
+
 # Define path for the Word2Vec model
 WORD2VEC_DIR = os.path.join(PROJECT_ROOT, 'output', 'Word2Vec_model')
