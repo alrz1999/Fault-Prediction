b8389ed6b5638cfa88555dab0dc5ef540fcd79b5
Alireza Ziaee <alrz1999@gmail.com>
2023-11-18 22:57:52 +0330

    Update PipelineStage

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -73,6 +73,12 @@ class PipelineStage:
     def process(self):
         pass
 
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
 
 class Pipeline:
     def __init__(self, stages):
