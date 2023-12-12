c62569f8d29823d4b54e175d4014e2f41d02ad0a
Alireza Ziaee <alrz1999@gmail.com>
2023-12-12 22:47:34 +0330

    Add precision recall curve

--- classification/evaluation/evaluation.py
+++ classification/evaluation/evaluation.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_auc_score, \
-    balanced_accuracy_score, matthews_corrcoef, roc_curve, auc
+    balanced_accuracy_score, matthews_corrcoef, roc_curve, auc, precision_recall_curve
 
 
 def evaluate(true_labels, predicted_probabilities):
@@ -12,14 +12,30 @@ def evaluate(true_labels, predicted_probabilities):
     fpr, tpr, thresholds = roc_curve(true_labels, predicted_probabilities)
     roc_auc = auc(fpr, tpr)
 
+    # Calculate Precision-Recall curve
+    precision, recall, _ = precision_recall_curve(true_labels, predicted_probabilities)
+
     # Plot ROC curve
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(12, 5))
+
+    # Subplot for ROC curve
+    plt.subplot(1, 2, 1)
     plt.plot(fpr, tpr, color='blue', lw=2, label=f'ROC curve (AUC = {roc_auc:.2f})')
     plt.plot([0, 1], [0, 1], color='gray', linestyle='--')
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic (ROC) Curve')
     plt.legend(loc='lower right')
+
+    # Subplot for Precision-Recall curve
+    plt.subplot(1, 2, 2)
+    plt.plot(recall, precision, color='green', lw=2, label='Precision-Recall curve')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-Recall Curve')
+    plt.legend(loc='best')
+
+    plt.tight_layout()
     plt.show()
 
     accuracy = accuracy_score(true_labels, predicted_labels)
